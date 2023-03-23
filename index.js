02e69814164056db34e325740dc0588f058369a9
Ruth <rwangare124@gmail.com>
2023-03-23 10:34:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// Code your solutions in this file
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+
+countDown(10); // counts down from 10 to 0
+
 function writeCards(names, event) {
-    const messages = [];
-    for (let i = 0; i < names.length; i++) {
-      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
-    return messages;
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
-  function countDown(number) {
-    while (number >= 0) {
-      console.log(number);
-      number--;
-    }
-  }
-  module.exports = { countDown };
+  return messages;
+}
